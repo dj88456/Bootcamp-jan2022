c775e195c699dc751bf01a97d5056312106dd0df
Alex Dai <daijian.gov@gmail.com>
2022-01-17 21:33:06 -0700

    Create learn.py

---
+++ learn.py
@@ -0,0 +1,35 @@
+if 5 > 2:
+    print("Five is greater than two!")
+
+x = 5
+x = "sally"
+# This is a comment.
+# comment two
+y = "Hello hello"  # 22
+"""
+is case dd 
+sad
+sda
+"""
+x = str(3)
+y = int(3.0)
+z = float(3.155)
+X = "sally"
+print(type(x))
+print(y)
+print(z)
+
+x = 'John'
+myFirstVar = "John"
+x = y = z = "orange"
+print(x)
+print(y)
+print(z)
+
+fruit = ["apple", "banana", "cherry"]
+x, y, z = fruit
+print(x)
+print(y)
+print(z)
+
+x = 
