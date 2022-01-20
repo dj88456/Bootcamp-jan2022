271ba8f6dbb03b7934c38174ed92de7e66a8e9b1
Alex Dai <daijian.gov@gmail.com>
2022-01-19 19:51:34 -0700

    Update learn.py

--- learn.py
+++ learn.py
@@ -32,4 +32,35 @@ print(x)
 print(y)
 print(z)
 
-x = 
+x = "awesome!"
+print("Python is " + x)
+
+x = "Python is "
+y = "awesome"
+z = x + y
+print(z)
+
+x = 5
+y = 10
+print(x + y)
+
+x = "awesome"
+
+
+def myfunc():
+    print("Python is" + x)
+
+
+myfunc()
+
+x = "awesome"
+
+
+def myfunc():
+    x = "fantastic"
+    print("Python is " + x)
+
+
+myfunc()
+
+print("Python is " + x)
